bbe2656b71d654fde67c6a8e15059bf94f124b71
arjunvir sidhu <97191137+rjnvr@users.noreply.github.com>
2022-09-13 14:55:39 -0400

    Trading Bot -- Working (user quantity input not done)

--- tradingBot.py
+++ tradingBot.py
@@ -1,5 +1,5 @@
 #Imports
-from re import A
+#
 import ibapi
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
@@ -16,6 +16,8 @@ import time
 
 #Vars
 orderId = 1
+
+
 #Class for connnecting to IBKR
 class IBApi(EClient, EWrapper):
     def __init__(self):
@@ -26,7 +28,7 @@ class IBApi(EClient, EWrapper):
             bot.on_bar_update(reqId, bar, False)
         except Exception as e:
             print(e)
-    
+
     #On Realtime Bar after historical data finishes
     def historicalDataUpdate(self, reqId, bar):
         try: 
@@ -35,7 +37,9 @@ class IBApi(EClient, EWrapper):
             print(e)
     #On Historical data ends
     def historicalDataEnd(self, reqId, start, end):
+        print("ReqID : below")
         print(reqId)
+        
 
     #Get next order ID
     def nextValidId(self, nextorderId):
@@ -53,6 +57,8 @@ class IBApi(EClient, EWrapper):
         def error(self, id, errorCode, errorMsg):
             print(errorCode)
             print(errorMsg)
+
+   
 
 
 #Bar Object
@@ -76,7 +82,7 @@ class Bar:
 #Bot Logic
 class Bot:
     ib = None
-    barsize = "1 Hour"
+    barsize = 1
     currentBar = Bar()
     bars = []
     reqId = 1
@@ -84,12 +90,14 @@ class Bot:
     ema1Period = 9
     ema2Period = 21
     symbol = ""
-    initalbartime = datetime.now().astimezone(pytz.timezone("America/New_York"))
+    initialbartime = datetime.now().astimezone(pytz.timezone("America/New_York"))
+
 
 
     def __init__(self):
         #Connect to IBKR on init
         self.ib = IBApi()
+        # 4001 = Gateway port  -- 7496 = TWS port
         self.ib.connect("127.0.0.1", 7496, 1)
     
         #threading
@@ -99,24 +107,28 @@ class Bot:
 
         currentBar = Bar()
 
-
-
-
         #Get Symbol/Ticker
         self.symbol = input("Enter the symbol/ticker you want to trade: ")
 
         #Get bar size
         #Valid bar sizes are: _ secs, 1 min, _ mins, 1 hour, _ hours, 1 day, 1 week, 1 month
-    
-        # self.barsize = input("Enter the barsize you want to trade in minutes: ")
-        # mintext = " min"
-        # if (int(self.barsize) > 1):
-        #     mintext = " mins"
-        # elif (int(self.barsize) >= 60):
-        #     mintext = " hour"
-        
+        self.barsize = input("Enter the barsize you want to trade in minutes : ")
+        text = " min"
+        if (int(self.barsize) > 1 and int(self.barsize) < 60):
+            text = " mins"
+        elif (int(self.barsize) == 60):
+            text = " hour"
+        elif (int(self.barsize) >= 120 and int(self.barsize) < 1440):
+            text = " hours"
+        elif (int(self.barsize) == 1440):
+            text = " day"
+        elif (int(self.barsize) > 1440):
+            text = " days"
+
+
         
         queryTime = (datetime.now().astimezone(pytz.timezone("America/New_York"))-timedelta(days=1)).replace(hour=16,minute=0,second=0,microsecond=0).strftime("%Y%m%d %H:%M:%S")
+        print("Querytime complete")
 
 
         #Create contract/Stock object to store stock info
@@ -128,7 +140,7 @@ class Bot:
 
         #Request Market Data
         #self.ib.reqRealTimeBars(0, contract, 3600, "MIDPOINT", 1, []) # 1 = Regular Trading Hours - 0 = all trading hours
-        self.ib.reqHistoricalData(self.reqId,contract,"","1 M",str(self.barsize),"MIDPOINT",1,1,True,[])
+        self.ib.reqHistoricalData(self.reqId,contract,"","2 D",str(self.barsize)+text,"MIDPOINT",0,1,True,[])
 
     #threading cont
     #Listen to socket in seperate thread
@@ -136,7 +148,7 @@ class Bot:
         self.ib.run()
 
     # Order setup
-    def MarketOrder(self, action, quantity):
+    def marketOrder(self, action, quantity):
         #Create contract/Stock object to store stock info
         contract = Contract()
         contract.symbol = self.symbol.upper()
@@ -149,6 +161,8 @@ class Bot:
         order.orderType = "MKT"
         order.totalQuantity = quantity
 
+        return order
+
 
 
 
@@ -159,12 +173,14 @@ class Bot:
         if (realtime == False):
             self.bars.append(bar)
         else:
-            bartime = datetime.strptime(bar.date, "%Y%m%d %H:%M:%S").asttimezone(pytz.timezone("America/New_York"))
-            minutes_diff = (bartime-self.initalbartime).total_seconds() / 60.0
+            bartime = datetime.strptime(bar.date, "%Y%m%d %H:%M:%S").astimezone(pytz.timezone("America/New_York"))
+            print("getting bar data")
+            minutes_diff = (bartime-self.initialbartime).total_seconds() / 60.0
             self.currentBar.date = bartime
             lastBar = self.bars[len(self.bars)-1]
             #On Bar Close
-            if (minutes_diff > 0 and math.floor(minutes_diff) % self.barsize == 0):
+            if (minutes_diff > 0 and math.floor(minutes_diff) % int(self.barsize) == 0):
+                self.initialbartime = bartime
                 #Entry 
                 closes = []
                 for bar in self.bars:
@@ -172,40 +188,59 @@ class Bot:
                 
                 #first EMA (9 period ema)
                 self.close_array = pd.Series(np.asarray(closes))
-                self.ema1 = ta.trend.EMAIndicator(self.close_array, self.ema1Period, True)
+                self.ema1 = ta.trend.ema_indicator(self.close_array, self.ema1Period, True)
                 print("EMA9 : " + str(self.ema1[len(self.ema1)-1]))
                 #second EMA (21 period ema)
                 self.close_array = pd.Series(np.asarray(closes))
-                self.ema2 = ta.trend.EMAIndicator(self.close_array, self.ema2Period, True)
+                self.ema2 = ta.trend.ema_indicator(self.close_array, self.ema2Period, True)
                 print("EMA21 : " + str(self.ema2[len(self.ema2)-1]))
 
+                print("Criteria check")
                 #Criteria check
-                if (self.ema1 > self.ema2):
+                if (str(self.ema1[len(self.ema1)-1]) > str(self.ema2[len(self.ema2)-1])):
+                    print("BUY ORDER EXECUTING")
                     #Order Buy
                     quantity = 1
-                    mktOrder = self.MarketOrder("BUY", quantity)
-                    self.ib.placeOrder(self.nextOrderId(), self.contract, mktOrder)
+                    mktOrder = self.marketOrder("BUY", quantity)
                     contract = Contract()
                     contract.symbol = self.symbol.upper()
                     contract.secType = "STK"
                     contract.exchange = "SMART"
                     contract.currency = "USD"
+                    self.ib.placeOrder(orderId, contract, mktOrder)
+                    orderId += 1
                     
                 
-                if (self.ema2 > self.ema1):
+                if (str(self.ema2[len(self.ema2)-1]) > str(self.ema1[len(self.ema1)-1])):
+                    print("SELL ORDER EXECUTING")
                     #Order Sell
                     quantity = 1
-                    mktOrder = self.MarketOrder("SELL", quantity)
+                    mktOrder = self.marketOrder("SELL", quantity)
                     contract = Contract()
                     contract.symbol = self.symbol.upper()
                     contract.secType = "STK"
                     contract.exchange = "SMART"
                     contract.currency = "USD"
+                    self.ib.placeOrder(orderId, contract, mktOrder)
+                    orderId += 1
                 
-                self.ib.placeOrder(self.nextOrderId(), self.contract, mktOrder)
-                orderId += 1
+                
+                
 
-                
+                #Bar closed append 
+                self.currentBar.close = bar.close
+                if (self.currentBar.date != lastBar.date):
+                    print("New bar")
+                    self.bars.append(self.currentBar)
+                self.currentBar.open = bar.open
+        
+        #build realtime bar
+        if (self.currentBar.open == 0):
+            self.currentBar.open = bar.open
+        if (self.currentBar.high == 0 or bar.high > self.currentBar.high):
+            self.currentBar.high = bar.high
+        if (self.currentBar.low == 0 or bar.low > self.currentBar.low):
+            self.currentBar.low = bar.low
                 
                         
 
